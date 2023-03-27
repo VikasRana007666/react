e9eafacd0005279a0613e38a936b32c8709a6792
VikasRana007666 <cookthewebpage@gmail.com>
2023-03-28 00:28:28 +0530

    pages/post created

---
+++ next-app-2/pages/post/[slug]/index.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function SinglePost() {
+  return (
+    <>
+      <h1>Single Post</h1>
+    </>
+  );
+}
---
+++ next-app-2/pages/post/index.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Post() {
+  return (
+    <>
+      <h1>Posts</h1>
+    </>
+  );
+}
