be0a826527fcad5306889f5935cd348f6f5e370a
VikasRana007666 <cookthewebpage@gmail.com>
2023-04-01 01:42:18 +0530

    The Root Route

--- vite-react-app/src/main.jsx
+++ vite-react-app/src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "../src/routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
   },
 ]);
 
